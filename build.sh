db59ba62c81e716d827de0782f8447bba4783875
klin1344 <kevlin97@gmail.com>
2012-08-25 19:10:59 -0700

    revert to arm-eabi-4.4.3 toolchain

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-CROSS_COMPILE=/home/klin1344/aosp/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+CROSS_COMPILE=/home/klin1344/toolchains/arm-eabi-4.4.3/bin/arm-eabi-
 INITRAMFS_DIR=ramdisk.gz
 KERNEL_NAME=FusionUlt_4.0.4_Sense
 KERNEL_VNUMBER=1.1
